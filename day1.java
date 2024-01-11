8a6fbf47b24acc00e9a95845838ff8a10231cb16
Anuska Palit <113778308+its-anuskapalit@users.noreply.github.com>
2024-01-11 20:55:14 +0530

    Create day1.java

---
+++ day1.java
@@ -0,0 +1,21 @@
+//Day 1
+class online{
+    public static void main(String args[]){
+        int num=5;
+        System.out.println(num);
+        int num1=5;
+        int num2=10;
+        System.out.println(num1+num2);
+        float num3= 10.5f;
+        byte num4=22;
+        Boolean val=true;
+        long num5=1111111111111111111L;
+        double num6=9999999d;
+        System.out.println(num3);
+    }
+}
+/*
+datatypes::
+primitives: Bool(T/F) char('') Integer(int-4byte,long-8,short-2,byte-1(8bits) floating point(float,double,-128<->127)
+non-primitives:: array class String
+ */
