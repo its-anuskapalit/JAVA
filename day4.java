dc926a6c2025240ae49204b647f12738a5d211ce
Anuska Palit <113778308+its-anuskapalit@users.noreply.github.com>
2024-01-24 20:52:27 +0530

    Create day4.java

---
+++ day4.java
@@ -0,0 +1,42 @@
+////Day 4
+//public class online{
+//    public static  void main(String args[]){
+//        for (int i=0;i<5;i++){
+//            for(int j=0;j<i;j++){
+//                System.out.print(j);
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+//
+//public class online{
+//    public static  void main(String args[]){
+//        for (int i=5;i>0;i--){
+//            for(int j=0;j<i;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+//public class online{
+//    public static  void main(String args[]){
+//        for (int i=0;i<5;i++){
+//            for(int j=0;j<i;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+//public class online{
+//    public static  void main(String args[]){
+//        for (int i=0;i<4;i++){
+//            for(int j=0;j<4;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
