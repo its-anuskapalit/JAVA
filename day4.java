c1a3deb6a2836e5edc39092f9bbca03c0ac3914c
Anuska Palit <113778308+its-anuskapalit@users.noreply.github.com>
2024-01-25 20:32:16 +0530

    Update day4.java

--- day4.java
+++ day4.java
@@ -40,3 +40,17 @@
 //        }
 //    }
 //}
+// class online {
+//     public static void main(String[] args)
+//     {
+//         for (int i = 1; i < 7; i++) {
+//             for (int j = 7; j > i; j--) {
+//                 System.out.print(" ");
+//             }
+//             for (int j = 1; j <i; j++) {
+//                 System.out.print("* ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
