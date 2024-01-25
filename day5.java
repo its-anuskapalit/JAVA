e01fb5346593e9646a100ae03732a92785eeddc2
Anuska Palit <113778308+its-anuskapalit@users.noreply.github.com>
2024-01-25 20:38:48 +0530

    Create day5.java

---
+++ day5.java
@@ -0,0 +1,42 @@
+//Day 5
+// ARRAYS: collection of values of same datatype
+//public class online{
+//    public  static  void main(String args[]){
+//       int a[]={1,5,9,6,7,4,5,6};
+//       for(int i=0;i< a.length;i++) {
+//           System.out.println(a[i]);
+//       }
+//       //or
+//        for(int values: a){
+//            System.out.println(values);
+//        }
+//       int arr[]=new int[5];
+//       arr[0]=10;
+//       arr[4]=100;
+//       System.out.println(arr[0]+arr[4]);
+//
+//    }
+//}
+
+//public class online{
+//    public  static  void main(String args[]){
+//        int roll[]={1,2,3,4,5,6,7,8,9,10};
+//        for(int i=0;i< roll.length;i++) {
+//            if (roll[i] % 2 == 0) {
+//                System.out.println(roll[i]);
+//            }
+//        }
+//    }
+//}
+
+
+//public class online{
+//    public  static  void main(String args[]){
+//        float arr[]={1.2f,5.6f,4.5f,1.3f,1.6f};
+//        float sum=0.0f;
+//        for(int i=0;i< arr.length;i++) {
+//            sum = sum + arr[i];
+//        }
+//        System.out.println(sum);
+//    }
+//}
