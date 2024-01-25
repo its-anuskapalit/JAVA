10d8871bf885e0d4adf70f81680319c1928fc97a
Anuska Palit <113778308+its-anuskapalit@users.noreply.github.com>
2024-01-25 20:55:37 +0530

    Update day5.java

--- day5.java
+++ day5.java
@@ -40,3 +40,15 @@
 //        System.out.println(sum);
 //    }
 //}
+
+// public class online {
+//     public static void main(String args[]) {
+//         int arr[][] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+//         for (int i = 0; i < arr.length; i++) {
+//             for (int j = 0; j < arr.length; j++) {
+
+//                 System.out.println(arr[i][j]);
+//             }
+//         }
+//     }
+// }
