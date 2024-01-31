610258ececca8df112a3d59342788586429d56e6
Anuska Palit <113778308+its-anuskapalit@users.noreply.github.com>
2024-01-31 20:52:29 +0530

    Create day6.java

---
+++ day6.java
@@ -0,0 +1,50 @@
+////DAY 6
+
+//public class online {
+//    public static void main(String args[]) {
+//        int arr[][] = {{1, 2, 3,4}, {4, 5, 6,4}, {7, 8, 9,4}};
+//        for (int i = 0; i < arr.length; i++) {
+//            for (int j = 0; j < arr[0].length; j++) {
+//
+//                System.out.print(arr[i][j]+" ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+
+
+//public class online {
+//    public static void main(String args[]) {
+//        int arr[][] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+//        for (int i = 0; i < arr.length; i++) {
+//            for (int j = 0; j < arr[0].length; j++) {
+//                if (i == 0 || i == arr.length - 1) {
+//                    System.out.print(arr[i][j] + " ");
+//                }
+//
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+
+
+////PRINT BORDER ELEMENTS
+//public class online {
+//    public static void main(String args[]) {
+//        int arr[][] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+//        for (int i = 0; i < arr.length; i++) {
+//            for (int j = 0; j < arr[0].length; j++) {
+//                if (i == 0 || i == arr.length - 1 || j == 0 || j == arr.length - 1) {
+//                    System.out.print(arr[i][j]);
+//                }
+//                else {
+//                    System.out.print(" ");
+//                }
+//            }
+//                System.out.println();
+//
+//        }
+//    }
+//}
