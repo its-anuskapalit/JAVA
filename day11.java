c3b395eb0901fe628ac432a5772225a8cdcd1e58
Anuska Palit <113778308+its-anuskapalit@users.noreply.github.com>
2024-02-09 19:31:20 +0530

    Create day11.java

---
+++ day11.java
@@ -0,0 +1,93 @@
+//DAY 10
+//class Student{
+//    private String name;
+//    private int rollNumber;
+//    private int marks;
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public int getRollNumber() {
+//        return rollNumber;
+//    }
+//
+//    public int getMarks() {
+//        return marks;
+//    }
+//
+//    public void setMarks(int marks) {
+//        this.marks = marks;
+//    }
+//
+//    public void setRollNumber(int rollNumber) {
+//        this.rollNumber = rollNumber;
+//    }
+//}
+//class online{
+//    public static void main(String args[]){
+//
+//    }
+//}
+//class Student{
+//    public String name;
+//    public int id;
+//    public static String uniName;
+//
+//    public void output(){
+//        System.out.println("Name : " + name + " ID : " + id + " university : " + uniName);
+//    }
+//}
+//
+//
+//class online{
+//    public static void main(String args[]){
+//        Student s1 = new Student();
+//        s1.name = "Aanand";
+//        s1.id = 1;
+//        s1.uniName = "LPU";
+//
+//        Student s2 = new Student();
+//        s2.name = "Azhar";
+//        s2.id = 2;
+//        s2.uniName = "LPU";
+//
+//        s1.output();
+//        s2.output();
+//    }
+//}
+
+// class Person{
+
+//     public String name;
+//     public int age;
+
+//     //default constructor
+//     public Person(){
+//         System.out.println("From Constructor method");
+//         name = "Hello";
+//         age = 20;
+//     }
+
+// }
+
+// class LPU{
+//     public static void main(String a[]){
+
+//         //Constructors
+
+//         Person p1 = new Person();
+//         Person p2 = new Person();
+//         Person p3 = new Person();
+
+
+//         // p1.age = 11;
+//         // System.out.println(p1.name);
+//         // System.out.println(p2.name);
+
+//     }
+// }
