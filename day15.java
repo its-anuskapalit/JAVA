87068435969e330d14ca497eb505df2fbd6813c3
Anuska Palit <113778308+its-anuskapalit@users.noreply.github.com>
2024-02-22 20:55:54 +0530

    Update day15.java

--- day15.java
+++ day15.java
@@ -158,3 +158,34 @@ public class online {
     }
 }
 //..................................................................................................
+class A{
+    public A(){ //a1
+        System.out.println("From A");
+    }
+
+    public A(int n){ //a2
+        System.out.println("From A int");
+    }
+
+    public A(int n1, int n2){
+        System.out.println("From A int twice");
+    }
+}
+
+class B extends A{
+    public B(){ //B1
+        this(5);
+        System.out.println("From B");
+    }
+
+    public B(int n){ //B2
+        System.out.println("From B int");
+    }
+}
+
+class LPU{
+    public static void main(String args[]){
+        
+        A a1 = new B();
+    }
+}
