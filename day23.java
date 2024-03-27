dcab8c1c42d58a80d56cb031043257650b1bdff9
Anuska Palit <113778308+its-anuskapalit@users.noreply.github.com>
2024-03-27 20:51:20 +0530

    Create day23.java

---
+++ day23.java
@@ -0,0 +1,20 @@
+//day23
+//class online{
+//    public static void main(String args[]){
+//        try{
+//            int a=30/0;
+//            System.out.println(a);
+//        }
+//        catch(Exception e){
+//            System.out.println(e);
+//
+//        }
+//        finally{
+//            System.out.println("From finally block");
+//        }
+//        System.out.println("At the end");
+//    }
+//}
+------------------------------------------------------------------------------------
+//COLLECTIONS --> frameworks --> Interface & Classes
+//COLLECTION IS A INTERFACE --> ROOT INTERFACE OF COLLECTIONS FRAMEWORK HIERARCHY
