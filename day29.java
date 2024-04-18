7a2ce090dddc51ac0b75671ecc97dfa8e5e66916
Anuska Palit <113778308+its-anuskapalit@users.noreply.github.com>
2024-04-18 16:52:03 +0530

    Create day29.java

---
+++ day29.java
@@ -0,0 +1,36 @@
+//day29
+//generics
+//class Mygeneric<T>{
+//    T data;
+//    Mygeneric(T data){
+//        this.data=data;
+//    }
+//    T getdata(){
+//        return this.data;
+//    }
+//}
+//class online {
+//    public static void main(String args[])
+//    {
+//        Mygeneric ob=new Mygeneric(5);
+//        Mygeneric ob1=new Mygeneric("Hello");
+//        System.out.println(ob.getdata());
+//        System.out.println(ob1.getdata());
+//
+//    }
+//}
+
+//class Mygeneric<T extends Number>{
+//    void print(){
+//        System.out.println("hello");
+//    }
+//}
+//class online {
+//    public static void main(String args[])
+//    {
+//        Mygeneric ob=new Mygeneric();
+//        ob.print();
+//
+//    }
+//}
+//code reuseibility
