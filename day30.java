e7d9324f47edaf4d86111592df8dee55dd4b3ab9
Anuska Palit <113778308+its-anuskapalit@users.noreply.github.com>
2024-04-18 19:55:50 +0530

    Create day30.java

---
+++ day30.java
@@ -0,0 +1,36 @@
+//day30
+
+//import java.io.File;
+//import java.io.IOException;
+//
+//class LPU{
+//    public static void main(String args[]){
+//        try{
+//            File file = new File("MyFile.txt");
+//
+//            if(file.createNewFile()){
+//                System.out.println("File created successfully");
+//            }
+//            else{
+//                System.out.println("File already exists");
+//            }
+//        }
+//        catch(IOException e){
+//            System.out.println(e);
+//        }
+//    }
+//}
+
+//import java.io.File;
+//class LPU{
+//    public static void main(String args[]){
+//        File file = new File("MyFile.txt");
+//        if(file.exists()){
+//            System.out.println(file.getAbsoluteFile());
+//            System.out.println(file.canRead());
+//            System.out.println(file.canWrite());
+//            System.out.println(file.length());
+//        }
+//    }
+//}
+
